1f2431a600b157796d127bfb032a1101f6b6e1bf
anduser <anduser@DeveloperPC004.(none)>
2016-06-02 18:38:05 +0300

    4

--- content.js
+++ content.js
@@ -5,48 +5,56 @@
 
         });
 
-
-
-        var rect = null,
-        mouse = {
-            startX: 0,
-            startY: 0,
+        var viewRect = {
+            rect: null,
             x: 0,
             y: 0
-        }
+        },
+        rect = null;
 
         function setMousePosition(e) {
-            mouse.x = e.pageX;
-            mouse.y = e.pageY;
+            viewRect.x = e.pageX;
+            viewRect.y = e.pageY;
         }
 
         function onMouseMove () {
-            if (rect !== null) {
-                rect.model.x = (mouse.x - mouse.startX < 0) ? mouse.x + 'px' : mouse.startX + 'px';
-                rect.model.y = (mouse.y - mouse.startY < 0) ? mouse.y + 'px' : mouse.startY + 'px';
-                rect.model.width = Math.abs(mouse.x - mouse.startX) + 'px';
-                rect.model.height = Math.abs(mouse.y - mouse.startY) + 'px';
+            if (viewRect.rect !== null) {
+                rect.style.left = viewRect.rect.model.x = (viewRect.x - viewRect.startX < 0) ? viewRect.x + 'px' : viewRect.startX + 'px';
+                rect.style.top = viewRect.rect.model.y = (viewRect.y - viewRect.startY < 0) ? viewRect.y + 'px' : viewRect.startY + 'px';
+                rect.style.width = viewRect.rect.model.width = Math.abs(viewRect.x - viewRect.startX) + 'px';
+                rect.style.height = viewRect.rect.model.height = Math.abs(viewRect.y - viewRect.startY) + 'px';
             }
         }
 
        
         function onMouseDown () {
-            mouse.startX = mouse.x;
-            mouse.startY = mouse.y;
-            rect = {
+            viewRect.startX = viewRect.x;
+            viewRect.startY = viewRect.y;
+            viewRect.rect = {
                 name: 'update',
                 model: {}
             };
+            rect = document.createElement('div');
+            rect.className = 'viewRect';
+            rect.style.left = viewRect.x + 'px';
+            rect.style.top = viewRect.y + 'px';
+            document.body.appendChild(rect);
         }
 
         function onMouseUp () {
-            if (rect !== null) {
-                if (rect.model.width != "" && rect.model.width != "0px" && rect.model.width != undefined) {
-                    if (rect.model.height != "" && rect.model.height != "0px" && rect.model.height != undefined) {
+            if (viewRect.rect !== null) {
+                if (viewRect.rect.model.width != "" && viewRect.rect.model.width != "0px" && viewRect.rect.model.width != undefined) {
+                    if (viewRect.rect.model.height != "" && viewRect.rect.model.height != "0px" && viewRect.rect.model.height != undefined) {
                         port.postMessage({create: rect});
                     }
                 }
-                rect = null;
+                viewRect.rect = null;
+                var rectsToDelete = document.querySelectorAll('.viewRect');
+
+
+                for (var m = 0; m < rectsToDelete.length; m++) {
+                    document.body.removeChild(rectsToDelete[m]);
+                }
             }
         }
 
