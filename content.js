91e39549e48cfb2a8077bd9070d153f01ac51129
anduser <anduser@DeveloperPC004.(none)>
2016-06-07 16:21:30 +0300

    8

--- content.js
+++ content.js
@@ -1,120 +1,104 @@
 
-     function init() {
+function init() {
 
-        var rect = {
-            rect: null,
-            x: 0,
-            y: 0
+    var controller = {
+        view: null,
+        toggleSelection: function (isShift) {
+            document.body.className = isShift ? 'non-selectable' : '';
         },
-        viewRect = null;
+        create: function (isShift, x, y) {
+            if (!isShift) return;
 
-        function setMousePosition(e) {
-            rect.x = e.pageX;
-            rect.y = e.pageY;
-        }
-
-        function onMouseMove () {
-            if (rect.rect !== null) {
-                viewRect.style.left = rect.rect.model.x = (rect.x - rect.startX < 0) ? rect.x + 'px' : rect.startX + 'px';
-                viewRect.style.top = rect.rect.model.y = (rect.y - rect.startY < 0) ? rect.y + 'px' : rect.startY + 'px';
-                viewRect.style.width = rect.rect.model.width = Math.abs(rect.x - rect.startX) + 'px';
-                viewRect.style.height = rect.rect.model.height = Math.abs(rect.y - rect.startY) + 'px';
-            }
-        }
-
-        function onMouseDown () {
-            rect.startX = rect.x;
-            rect.startY = rect.y;
-            rect.rect = {
-                name: 'update',
-                model: {}
+            controller.start = {
+                x: x,
+                y: y
             };
-            viewRect = document.createElement('div');
-            viewRect.className = 'viewRect';
-            viewRect.style.left = rect.x + 'px';
-            viewRect.style.top = rect.y + 'px';
-            document.body.appendChild(viewRect);
+            controller.end = {
+                x: x,
+                y: y
+            };
+            controller.view = document.createElement('div');
+            controller.view.id = 'current-selection';
+            controller.view.className = 'viewRect';
+            document.body.appendChild(controller.view);
+
+        },
+        update: function (isShift, x, y) {
+            controller.end = {x: x, y: y};
+            if (controller.view != null) {
+                controller.refresh(isShift);
+            }
+        },
+        refresh: function (isShift) {
+            if (!isShift) return;
+
+            var model = controller.getModel();
+            controller.view.style.left = model.left + 'px';
+            controller.view.style.top = model.top + 'px';
+            controller.view.style.width = model.width + 'px';
+            controller.view.style.height = model.height + 'px';
+        },
+        getModel: function () {
+            return {
+                left: (controller.end.x - controller.start.x < 0) ? controller.end.x : controller.start.x,
+                top: (controller.end.y - controller.start.y < 0) ? controller.end.y : controller.start.y,
+                width: ~~(controller.end.x - controller.start.x),
+                height: ~~(controller.end.y - controller.start.y)
+            }
+        },
+        finish: function (isShift) {
+            if (!isShift) return;
+            controller.view = null;
+            controller.toggleSelection(false);
+            document.body.removeChild(document.getElementById('current-selection'));
+            chrome.runtime.sendMessage({type: 'update', model: controller.getModel()}, function (response) {
+                //render command is procceded here
+            });
         }
 
-        function onMouseUp () {
-            if (!_.isEmpty(rect.rect)) {
-                if (rect.rect.model.width != "" && rect.rect.model.width != "0px" && rect.rect.model.width != undefined) {
-                    if (rect.rect.model.height != "" && rect.rect.model.height != "0px" && rect.rect.model.height != undefined) {
-                        chrome.runtime.sendMessage({type: rect.rect}, function(response) {});
-                    }
-                }
-                rect.rect = null;
-                var rectsToDelete = document.querySelectorAll('.viewRect');
-                for (var m = 0; m < rectsToDelete.length; m++) {
-                    document.body.removeChild(rectsToDelete[m]);
-                }
-            }
+    };
+
+    document.body.onkeydown = function (e) {
+        controller.toggleSelection(e.shiftKey);
+    };
+
+    document.body.onmousedown = function (e) {
+        controller.create(e.shiftKey, e.pageX, e.pageY)
+    };
+
+    document.body.onmousemove = function (e) {
+        controller.update(e.shiftKey, e.pageX, e.pageY)
+    };
+
+    document.body.onmouseup = function (e) {
+        controller.finish(e.shiftKey, e);
+    };
+
+    document.body.onkeyup = function (e) {
+        controller.toggleSelection(false);
+    };
+
+}
+
+
+function render(model) {
+    var oldRects = document.querySelectorAll('.rectangle');
+    if (oldRects) {
+        for (var k = 0; k < oldRects.length; k++) {
+            document.body.removeChild(oldRects[k]);
         }
-
-        document.body.onmousemove = function (e) {
-            setMousePosition(e);
-            onMouseMove();
-        };
-
-        document.body.onkeydown = function (e) {
-            if (e.shiftKey) {
-                document.body.className = 'non-selectable';
-            }
-        };
-
-        document.body.onmousedown = function(e) {
-            if (e.shiftKey) {
-                onMouseDown ();
-            }
-        };
-
-        document.body.onmouseup = function (e) {
-            if (e.shiftKey) {
-                onMouseUp ();
-            }
-        };
-
-        document.body.onkeyup = function (e) {
-            if (!e.shiftKey) {
-                document.body.className = '';
-            }
-        };
-
-
-         /*if(request.model)
-             render(request.model);*/
-
-
-
+    }
+    for (var i = 0; i < model.model.length; i++) {
+        var restoredRect = document.createview('div');
+        restoredRect.className = 'rectangle';
+        restoredRect.style.left = model.model[i].x;
+        restoredRect.style.top = model.model[i].y;
+        restoredRect.style.width = model.model[i].width;
+        restoredRect.style.height = model.model[i].height;
+        document.body.appendChild(restoredRect);
     }
 
+}
 
+init();
 
-    function render(model) {
-        var oldRects = document.querySelectorAll('.rectangle');
-        if (oldRects) {
-            for (var k = 0; k < oldRects.length; k++) {
-                document.body.removeChild(oldRects[k]);
-            }
-        }
-        for (var i = 0; i < model.model.length; i++) {
-            var restoredRect = document.createElement('div');
-            restoredRect.className = 'rectangle';
-            restoredRect.style.left = model.model[i].x;
-            restoredRect.style.top = model.model[i].y;
-            restoredRect.style.width = model.model[i].width;
-            restoredRect.style.height = model.model[i].height;
-            document.body.appendChild(restoredRect);
-        }
-
-    }
-
-
-    //init();
-
-     chrome.runtime.onMessage.addListener(
-         function(request, sender, sendResponse) {
-             if(request.toggle)
-                 init();
-         return true;
-     });
