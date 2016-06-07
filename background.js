ac6e5e1146701f5c66c56a744f2ef8b7f40fe1bf
anduser <anduser@DeveloperPC004.(none)>
2016-06-07 16:21:23 +0300

    8

--- background.js
+++ background.js
@@ -9,41 +9,14 @@ function setup() {
             chrome.tabs.executeScript(null, { file: "lodash.js" }, function() {
                 chrome.tabs.executeScript(null, { file: "content.js" })
             });
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, {toggle: true}, function(response) { });
-            });
         }
     });
-
-    chrome.browserAction.onClicked.addListener(function(tab) {
-        chrome.tabs.insertCSS(null, {file: "mainStyles.css"});
-        chrome.tabs.executeScript(null, { file: "lodash.js" }, function() {
-            chrome.tabs.executeScript(null, { file: "content.js" })
-        });
-        toggle = !toggle;
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            if(toggle){
-                chrome.browserAction.setIcon({path: "images/icon.png"});
-                chrome.tabs.sendMessage(tabs[0].id, {toggle: toggle}, function(response) {
-                    console.log(toggle);
-                });
-            }
-            else{
-                chrome.browserAction.setIcon({path: "images/off.png"});
-                chrome.tabs.sendMessage(tabs[0].id, {toggle: toggle}, function(response) {
-                    console.log(toggle);
-                });
-            }
-        });
-
-    });
-
 
     chrome.runtime.onMessage.addListener(
         function(request, sender, sendResponse) {
 
-            if (request.type){
-                update(request.type);
+            if (request.type == 'update'){
+                update(request.model);
         }
         return true;
 
@@ -52,7 +25,6 @@ function setup() {
 }
 
 function update(newRectangle) {
-    var model = _.clone(newRectangle.model);
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
         updateModel(tabs[0].url);
     });
@@ -60,13 +32,13 @@ function update(newRectangle) {
     function updateModel(url) {
         var link = url;
         if (_.isEmpty(config)) {
-            config[link] = [model];
+            config[link] = [newRectangle.model];
         } else {
             for (key in config) {
                 if (key === link) {
-                    config[key].push(model);
+                    config[key].push(newRectangle.model);
                 } else {
-                    config[link] = [model];
+                    config[link] = [newRectangle.model];
                 }
             }
         }
